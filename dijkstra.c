9dd50c06256b5dda34eebda24041df8433e332e2
root <root@dj2.dk>
2017-05-14 18:41:56 +0000

    dati statici

--- dijkstra.c
+++ dijkstra.c
@@ -1,23 +1,62 @@
-#include<stdio.h>
-#define INFINITY 9999
+#include <stdio.h>
+#include <string.h>
+#define INFINITY 99999
 #define MAX 10
  
 void dijkstra(int G[MAX][MAX],int n,int startnode);
+
+
+char nomi_stazioni[5][10];
  
 int main()
 {
-    int G[MAX][MAX],i,j,n,u;
-    printf("Enter no. of vertices:");
-    scanf("%d",&n);
-    printf("\nEnter the adjacency matrix:\n");
+    int G[MAX][MAX],i,j,n,partenza;
+    n = 5; // da leggere dal file
     
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            scanf("%d",&G[i][j]);
+    strcpy(nomi_stazioni[0],"Pesaro");
+    strcpy(nomi_stazioni[1],"Bologna");
+    strcpy(nomi_stazioni[2],"Ancona");
+    strcpy(nomi_stazioni[3],"Torino");
+    strcpy(nomi_stazioni[4],"Milano");
+
+    // Pesaro
+    G[0][0] = 0; // Pesaro
+    G[0][1] = 158; // Bologna
+    G[0][2] = 60; // Ancona
+    G[0][3] = 0; // Torino
+    G[0][4] = 400; // Milano
     
-    printf("\nEnter the starting node:");
-    scanf("%d",&u);
-    dijkstra(G,n,u);
+    // Bologna
+    G[1][0] = 158; // Pesaro
+    G[1][1] = 0; // Bologna
+    G[1][2] = 210; // Ancona
+    G[1][3] = 0; // Torino
+    G[1][4] = 202; // Milano
+    
+    // Ancona
+    G[2][0] = 0; // Pesaro
+    G[2][1] = 0; // Bologna
+    G[2][2] = 0; // Ancona
+    G[2][3] = 505; // Torino
+    G[2][4] = 412; // Milano
+
+    // Torino
+    G[3][0] = 415; // Pesaro
+    G[3][1] = 0; // Bologna
+    G[3][2] = 505; // Ancona
+    G[3][3] = 0; // Torino
+    G[3][4] = 140; // Milano
+
+    // Milano
+    G[4][0] = 400; // Pesaro
+    G[4][1] = 0; // Bologna
+    G[4][2] = 0; // Ancona
+    G[4][3] = 0; // Torino
+    G[4][4] = 0; // Milano
+    
+    partenza = 0; // Pesaro
+
+    dijkstra(G,n,partenza);
     
     return 0;
 }
@@ -78,14 +117,15 @@ void dijkstra(int G[MAX][MAX],int n,int startnode)
     for(i=0;i<n;i++)
         if(i!=startnode)
         {
-            printf("\nDistance of node%d=%d",i,distance[i]);
-            printf("\nPath=%d",i);
+            printf("\nDistance of %s (%d)=%d",nomi_stazioni[i],i,distance[i]);
+            printf("\nPath=%s",nomi_stazioni[i], i);
             
             j=i;
             do
             {
                 j=pred[j];
-                printf("<-%d",j);
+                printf("<-%s",nomi_stazioni[j]);
             }while(j!=startnode);
     }
+    printf("\n");
 }
